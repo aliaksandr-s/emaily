3493c645799dea8a39bdce03fe545d650dd06293
Alex S <s-a-n-9@mail.ru>
2018-09-14 15:06:04 +0300

    Add prod and dev env

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,20 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose  = require('mongoose');
+
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
 
 passport.use(
   new GoogleStrategy(
@@ -10,7 +24,14 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log(accessToken, refreshToken, profile, done);
+      User.findOne({ googleId: profile.id }).then((existingUser) => {
+        if (!existingUser) {
+          new User({ googleId: profile.id})
+            .save()
+            .then(user => done(null, user));
+        }
+        done(null, existingUser);
+      });
     }
   )
 );
